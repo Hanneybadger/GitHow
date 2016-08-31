7605228933f202a401719f687dbeacb1bc3198cc
Hanneybadger <hwillen@gmail.com>
2016-08-31 07:35:28 +0200

    Made a counter that shows who pays what
    …during our stay in barcelona.

---
+++ HotelRoom.py
@@ -0,0 +1,20 @@
+Hotel_Stay = 16794
+
+H_nights = 10
+E_nights = 10
+S_nights = 8
+D_nights = 7
+
+Total_nights = H_nights + E_nights + S_nights + D_nights
+
+Sum_per_night = Hotel_Stay / Total_nights
+
+Hannah = Sum_per_night * 10
+Elisabet = Sum_per_night * 10
+Sandra = Sum_per_night * 8
+Denise = Sum_per_night * 7
+
+print Hannah
+print Elisabet
+print Sandra
+print Denise
